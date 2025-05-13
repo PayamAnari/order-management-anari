a787333f9124de752f68ffc37a28c2cc6b82b9c3
Payam Anari <payamanari@Mac.home>
2025-05-13 19:49:28 +0200

    Add orders route to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { errorHandler } from './utils/error.js';
 import authRoutes from './routes/auth.route.js';
+import ordersRoutes from './routes/orders.route.js';
 
 
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(cookieParser());
 
 
 app.use('/auth', authRoutes);
+app.use('/orders', ordersRoutes);
 
 app.use((req, res, next) => {
     next(errorHandler(404, 'Route not found'));

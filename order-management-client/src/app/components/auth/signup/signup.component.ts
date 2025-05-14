663f1b2b46739b157a3270331dcb3a23289828fe
Payam Anari <payamanari@Mac.home>
2025-05-14 20:16:04 +0200

    feat(auth): add signup component for user registration with styling and logic

---
+++ order-management-client/src/app/components/auth/signup/signup.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent {
+  signupForm: FormGroup;
+  error: string | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.signupForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      this.authService.signup(this.signupForm.value).subscribe({
+        next: () => this.router.navigate(['/signin']),
+        error: (err) => (this.error = err.error.error || 'Signup failed'),
+      });
+    }
+  }
+
+
+}
